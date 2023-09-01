a3b989dcf5b3326542ceb915737753873a6d8964
Simon Sprouse <simonhsprouse@gmail.com>
2023-09-01 17:47:04 -0500

    finished building api function for movie id

--- TMDB_query_single.py
+++ TMDB_query_single.py
@@ -10,28 +10,37 @@ import requests
 import json
 
 
-title = "Lion King"
-title.replace(" ", "%")
+def getID(title):
+    title.replace(" ", "%")
+    
+    url = "https://api.themoviedb.org/3/search/movie?query={}&include_adult=false&language=en-US&page=1".format(title)
+    
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Y2JlYzFjZmFiNGExYjU2YzY0NjMyNGI1MTg5MTVlZiIsInN1YiI6IjY0ZjI1ZGNkZTBjYTdmMDBhZTM5YzdhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Mat5xSeWLIco-N24HGrTbBvtQMWM85sR1DHOrDezYQ0"
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    
+    response_text = response.text
+    # print(response_text)
+    
+    
+    y = json.loads(response_text)
+    
+    results = y['results']
+    
+    if len(results) > 0: 
+        best_match = y['results'][0]
+        ## print(best_match)
+    
+    movie_id = best_match['id']
+    genre_ids = best_match['genre_ids']
+    
+    print(movie_id, genre_ids)
 
-url = "https://api.themoviedb.org/3/search/movie?query={}&include_adult=false&language=en-US&page=1".format(title)
-
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Y2JlYzFjZmFiNGExYjU2YzY0NjMyNGI1MTg5MTVlZiIsInN1YiI6IjY0ZjI1ZGNkZTBjYTdmMDBhZTM5YzdhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Mat5xSeWLIco-N24HGrTbBvtQMWM85sR1DHOrDezYQ0"
-}
-
-response = requests.get(url, headers=headers)
+getID("The Lion King")
 
 
-response_text = response.text
-# print(response_text)
-
-
-y = json.loads(response_text)
-
-results = y['results']
-
-if len(results) > 0: 
-    best_match = y['results'][0]
-    print(best_match)
 
