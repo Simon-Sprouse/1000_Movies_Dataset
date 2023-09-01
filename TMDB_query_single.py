532527421bbd13d13506d1f3d422b659d8c4fb52
Simon Sprouse <simonhsprouse@gmail.com>
2023-09-01 17:55:07 -0500

    connected api to Feature Engineering Notebook

--- TMDB_query_single.py
+++ TMDB_query_single.py
@@ -11,6 +11,8 @@ import json
 
 
 def getID(title):
+    """ Sends GET request to TMDB and returns Id's for movie title """
+    
     title.replace(" ", "%")
     
     url = "https://api.themoviedb.org/3/search/movie?query={}&include_adult=false&language=en-US&page=1".format(title)
@@ -20,27 +22,22 @@ def getID(title):
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Y2JlYzFjZmFiNGExYjU2YzY0NjMyNGI1MTg5MTVlZiIsInN1YiI6IjY0ZjI1ZGNkZTBjYTdmMDBhZTM5YzdhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Mat5xSeWLIco-N24HGrTbBvtQMWM85sR1DHOrDezYQ0"
     }
     
+    ## GET Request to TMDB server
     response = requests.get(url, headers=headers)
-    
-    
     response_text = response.text
-    # print(response_text)
+    parse = json.loads(response_text)
+    results = parse['results'] ## parsed results return as Json/Dict
     
-    
-    y = json.loads(response_text)
-    
-    results = y['results']
-    
+    ## Select the movie that matches title the best
     if len(results) > 0: 
-        best_match = y['results'][0]
-        ## print(best_match)
+        best_match = results[0]
     
     movie_id = best_match['id']
     genre_ids = best_match['genre_ids']
     
-    print(movie_id, genre_ids)
+    return (movie_id, genre_ids)
 
-getID("The Lion King")
+print(getID("The Lion King"))
 
 
 
