cc2b08b4b6f36cc4fe31671454e9f89e83d414b6
Simon Sprouse <simonhsprouse@gmail.com>
2023-09-15 19:13:49 -0500

    got curse word api to work under the condition that the title is perfect

--- script_scrape.py
+++ script_scrape.py
@@ -8,18 +8,44 @@ Created on Fri Sep 15 12:07:19 2023
 
 import requests
 from bs4 import BeautifulSoup
-
-url = "https://www.springfieldspringfield.co.uk/movie_script.php?movie=up"
-
-response = requests.get(url)
-# print(response.text)
+import string
 
 
-soup = BeautifulSoup(response.text, 'html.parser')
+def getScript(title):
 
-container = soup.findAll('div', class_='scrolling-script-container')
-
-# for div in container:
-#     print(div.text)
+    title = str.lower(title.replace(" ", '-'))
     
-print(container[0].text)
+    ## url = "https://www.springfieldspringfield.co.uk/movie_script.php?movie={}".format(title)
+
+    url = 'https://www.springfieldspringfield.co.uk/movie_script.php?movie=the-wolf-of-wall-street'
+    response = requests.get(url)
+
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+    container = soup.findAll('div', class_='scrolling-script-container')
+    
+
+    for div in container:
+        ## print(div.text)
+        return div.text
+
+        
+    return ""
+        
+        
+script = getScript("Godfather-the")
+print(script)
+
+file = open("swear.txt")
+    
+swears = [line.rstrip('\n') for line in file.readlines()]
+print(swears)
+
+count = 0
+
+for word in script.split(sep=' '):
+    print(str.lower(word).rstrip(string.punctuation))
+    if str.lower(word).rstrip(string.punctuation) in swears:
+        count += 1
+
+print(count)
